8876aae4cd069ccb9617554972e6e84876c58391
MinseoKim-dev <49779552+MinseoKim-dev@users.noreply.github.com>
2022-03-07 17:29:56 +0900

    Time: 114 ms (58.23%), Space: 14 MB (77.61%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -3,15 +3,24 @@ class Solution:
         
         window_size = len(s1)
         perm_counter = Counter(s1)
+        matched = 0
         
         for i in range(len(s2)):
             if s2[i] in perm_counter:
+                if perm_counter[s2[i]] == 0:
+                    matched -= 1
                 perm_counter[s2[i]] -= 1
+                if perm_counter[s2[i]] == 0:
+                    matched += 1
             
             if i >= window_size and s2[i-window_size] in perm_counter:
+                if perm_counter[s2[i-window_size]] == 0:
+                    matched -= 1
                 perm_counter[s2[i-window_size]] += 1
+                if perm_counter[s2[i-window_size]] == 0:
+                    matched += 1
             
-            if all([perm_counter[l]==0 for l in perm_counter]):
+            if matched == len(perm_counter):
                 return True
         
         return False
